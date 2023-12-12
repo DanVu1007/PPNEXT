0ead0ef1e8e5ed3999c4e6d55d642332b0b3569b
DanVa <vuanhdan123@gmail.com>
2023-12-12 22:08:46 +0700

    Bài 64: Xây dựng UI phần Header #1

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -2,17 +2,30 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Header.module.scss';
 import classNames from 'classnames/bind';
-
+import images from '~/assets/images';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 const cx = classNames.bind(styles);
-Header.propTypes = {
-
-};
+Header.propTypes = {};
 
 function Header(props) {
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
-                
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="logo" />
+                </div>
+                <div className={cx('search')}>
+                    <input type="text" placeholder="Tìm kiếm" spellCheck={false} />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('actions')}></div>
             </div>
         </header>
     );
